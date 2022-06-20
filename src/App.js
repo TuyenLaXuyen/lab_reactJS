9222c691811571bdbe65065edcef777fe0aa9979
Truong Ngoc Tuyen <tuyenfx16598@funix.edu.vn>
2022-06-20 19:45:42 +0700

    update lab02.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Button } from "reactstrap";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button color="danger">Danger!</Button>
     </div>
   );
 }
