30f2dc6d93c82bb47dd3e9bf5867c272d442912f
Truong Ngoc Tuyen <tuyenfx16598@funix.edu.vn>
2022-07-01 14:17:03 +0700

    'lab03_2'

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Navbar, NavbarBand } from "reactstrap";
 import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
-            {/* <NavbarBand href="/">Ristorante Con Fusion</NavbarBand> */}
+            <p>Ristorante Con Fusion</p>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
