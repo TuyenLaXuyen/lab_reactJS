57956f424f627f41eb1a0b5a46a3caf7f61e1a52
Truong Ngoc Tuyen <tuyenfx16598@funix.edu.vn>
2022-07-07 16:35:53 +0700

    update lan 7

--- src/App.js
+++ src/App.js
@@ -20,7 +20,10 @@ class App extends Component {
         <Navbar dark color="primary">
           <div className="container text-start ">
             <h4>Ứng dụng quản lý nhân sự v1.0</h4>
-            Số cột : <input id="input-col" />
+            Số cột : <input id="input-col" />{" "}
+            <button id="btn-save" className="btn btn-success">
+              Save
+            </button>
           </div>
         </Navbar>
         <StaffList staffs={this.state.staffs} />
@@ -28,6 +31,10 @@ class App extends Component {
     );
   }
 }
-const col = document.getElementById("input-col").value;
-console.log(col);
+
 export default App;
+const col = document.getElementById("input-col");
+const saveBtn = document.getElementById("btn-save");
+saveBtn.addEventListener("click", function () {
+  console.log("abc");
+});
