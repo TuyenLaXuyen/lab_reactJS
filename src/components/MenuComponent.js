92d735c5573805416f141e262cbb9d200b0abf91
Truong Ngoc Tuyen <tuyenfx16598@funix.edu.vn>
2022-07-08 18:10:33 +0700

    update lan 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -10,7 +10,7 @@ import {
 
 function RenderMenuItem({ dish, onClick }) {
   return (
-    <Card onClick={() => this.props.onClick(dish.id)}>
+    <Card onClick={() => onClick(dish.id)}>
       <CardImg width="100%" src={dish.image} alt={dish.name} />
       <CardImgOverlay>
         <CardTitle>{dish.name}</CardTitle>
@@ -19,8 +19,12 @@ function RenderMenuItem({ dish, onClick }) {
   );
 }
 const Menu = (props) => {
-  const menu = this.props.dishes.map((dish) => {
-    return <div key={dish.id} className="col-12 col-md-5 m-1"></div>;
+  const menu = props.dishes.map((dish) => {
+    return (
+      <div key={dish.id} className="col-12 col-md-5 m-1">
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
+      </div>
+    );
   });
 
   return (
