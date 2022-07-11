2ffa6c1912e53ecc6169e017703408923f8e35f2
Truong Ngoc Tuyen <tuyenfx16598@funix.edu.vn>
2022-07-11 23:00:24 +0700

    update navbar

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,15 +1,71 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBand, Jumbotron } from "reactstrap";
+import {
+  Navbar,
+  NavbarBand,
+  Jumbotron,
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false,
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <Navbar dark>
+        <Navbar dark expand="md">
           <div className="container">
-            <p>Ristorante Con Fusion</p>
+            <NavbarToggler onClick={this.toggleNav} />
+            <div className="mr-auto" href="/">
+              <img
+                src="assets/images/logo.png"
+                height="30"
+                width="41"
+                alt="Ristorante Con Fusion"
+              />
+            </div>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <Link className="nav-link" to="/home">
+                    <span className="fa fa-home fa-lg"></span> Home
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link className="nav-link" to="/aboutus">
+                    <span className="fa fa-info fa-lg"></span> About us
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link className="nav-link" to="/menu">
+                    <span className="fa fa-list fa-lg"></span> Menu
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link className="nav-link" to="/contactus">
+                    <span className="fa fa-address-card fa-lg"></span> Contact
+                  </Link>
+                </NavItem>
+              </Nav>
+            </Collapse>
           </div>
         </Navbar>
+
         <div>
           <div className="container">
             <div className="row row-header">
